6b7677f09fe197a2b5aa1db136f8d0475876f11a
Yuuto <Jasonehrlich72@gmail.com>
2024-10-07 16:11:38 +0200

    V-1.3 DEV

--- src/main/java/de/questcraft/plugins/elytraOnSpawn.java
+++ src/main/java/de/questcraft/plugins/elytraOnSpawn.java
@@ -1,14 +1,22 @@
 package de.questcraft.plugins;
 
+
+import de.questcraft.plugins.commands.elytraOnSpawnCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import de.questcraft.plugins.listener.SpawnBoostListener;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 public final class elytraOnSpawn extends JavaPlugin {
@@ -25,17 +33,36 @@ public final class elytraOnSpawn extends JavaPlugin {
         saveDefaultConfig();
         loadConfig();
 
-        verbose = config.getBoolean("verbose", false);
+        try {
+            verbose = config.getBoolean("verbose", false);
+        } catch (Exception e) {
+            verbose = true;
+        }
         log.info("Verbose mode: " + verbose);
 
         configCheck();
-
         getServer().getPluginManager().registerEvents(new SpawnBoostListener(this), this);
+        log.info("Loading elytraOnSpawn commands");
+        try {
+            final Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            bukkitCommandMap.setAccessible(true);
+            CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
+
+            PluginCommand command = getCommand("elytraOnSpawn");
+            if (command == null) {
+                command = new PluginCommand("elytraOnSpawn", this);
+                commandMap.register(getName(), command);
+            }
+            command.setExecutor(new elytraOnSpawnCommand(this));
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        log.info("Commands loaded!");
 
         log.info("ElytraOnSpawn is ready!");
     }
 
-    private void loadConfig() {
+    public void loadConfig() {
         File configFile = new File(getDataFolder(), "config.yml");
         config = YamlConfiguration.loadConfiguration(configFile);
     }
@@ -66,34 +93,26 @@ public final class elytraOnSpawn extends JavaPlugin {
             } else {
                 Object value = config.get(path);
                 boolean isValid = true;
-                String typeError = "";
-
-                switch (path) {
-                    case "verbose":
-                    case "boostSound":
-                    case "switchGamemodeCancelSound":
-                    case "particle":
+                String typeError = switch (path) {
+                    case "verbose", "boostSound", "switchGamemodeCancelSound", "particle" -> {
                         isValid = value instanceof Boolean;
-                        typeError = "should be true or false";
-                        break;
-                    case "spawnRadius":
-                    case "boostSoundVolume":
-                    case "boostSoundPitch":
-                    case "switchGamemodeCancelSoundVolume":
-                    case "switchGamemodeCancelSoundPitch":
+                        yield "should be true or false";
+                    }
+                    case "spawnRadius", "boostSoundVolume", "boostSoundPitch", "switchGamemodeCancelSoundVolume",
+                         "switchGamemodeCancelSoundPitch" -> {
                         isValid = value instanceof Number;
-                        typeError = "should be a number";
-                        break;
-                    case "flyBoostMultiplier":
-                    case "startBoostMultiplier":
+                        yield "should be a number";
+                    }
+                    case "flyBoostMultiplier", "startBoostMultiplier" -> {
                         isValid = value instanceof Number && ((Number) value).doubleValue() > 0;
-                        typeError = "should be a positive number";
-                        break;
-                    case "world":
+                        yield "should be a positive number";
+                    }
+                    case "world" -> {
                         isValid = value instanceof String;
-                        typeError = "should be a string";
-                        break;
-                }
+                        yield "should be a string";
+                    }
+                    default -> "";
+                };
 
                 if (!isValid) {
                     log.severe("Invalid configuration for " + path + ": " + typeError);
@@ -113,7 +132,7 @@ public final class elytraOnSpawn extends JavaPlugin {
     }
 
     @Override
-    public FileConfiguration getConfig() {
+    public @NotNull FileConfiguration getConfig() {
         return config;
     }
 }
