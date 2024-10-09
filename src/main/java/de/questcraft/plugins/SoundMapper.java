32c165cfde6f63ade7ff1a9ea8ff6051a1c2631e
Yuuto <Jasonehrlich72@gmail.com>
2024-10-09 16:40:31 +0200

    chore: rename var

--- src/main/java/de/questcraft/plugins/SoundMapper.java
+++ src/main/java/de/questcraft/plugins/SoundMapper.java
@@ -6,19 +6,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SoundMapper {
-    private static final Map<String, Sound> vs2bsMap = new HashMap<>();
+    private static final Map<String, Sound> vanillaToBukkitMap = new HashMap<>();
 
     public static void initialize() {
         for (Sound bukkitSound : Sound.values()) {
             String vanillaSoundKey = bukkitSound.getKey().getKey();
-            vs2bsMap.put(vanillaSoundKey, bukkitSound);
+            vanillaToBukkitMap.put(vanillaSoundKey, bukkitSound);
         }
-        System.out.println("SoundMapper initialized with " + vs2bsMap.size() + " sounds.");
+        System.out.println("SoundMapper initialized with " + vanillaToBukkitMap.size() + " sounds.");
     }
 
     public static Sound getSound(String soundKey) {
         try {
-            Sound bukkitSound = vs2bsMap.get(soundKey);
+            Sound bukkitSound = vanillaToBukkitMap.get(soundKey);
             if (bukkitSound != null) {
                 return bukkitSound;
             }
