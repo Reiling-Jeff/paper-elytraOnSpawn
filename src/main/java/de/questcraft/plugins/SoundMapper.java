51d56a40f706ebf8360f6a872b71481d9795ff43
Yuuto <Jasonehrlich72@gmail.com>
2024-10-09 03:39:12 +0200

    refactor: update SoundMapper.java

--- src/main/java/de/questcraft/plugins/SoundMapper.java
+++ src/main/java/de/questcraft/plugins/SoundMapper.java
@@ -8,19 +8,24 @@ import java.util.Map;
 public class SoundMapper {
     private static final Map<String, Sound> vs2bsMap = new HashMap<>();
 
-    static {
+    public static void initialize() {
         for (Sound bukkitSound : Sound.values()) {
             String vanillaSoundKey = bukkitSound.getKey().getKey();
             vs2bsMap.put(vanillaSoundKey, bukkitSound);
         }
+        System.out.println("SoundMapper initialized with " + vs2bsMap.size() + " sounds.");
     }
 
-    public static Sound getSound(String soundKey) throws IllegalArgumentException {
+    public static Sound getSound(String soundKey) {
         try {
             Sound bukkitSound = vs2bsMap.get(soundKey);
-            return bukkitSound != null ? bukkitSound : Sound.valueOf(soundKey);
+            if (bukkitSound != null) {
+                return bukkitSound;
+            }
+            return Sound.valueOf(soundKey.toUpperCase().replace(".", "_"));
         } catch (IllegalArgumentException e) {
-            return null;
+            e.printStackTrace();
+            return Sound.ENTITY_CREEPER_HURT;
         }
     }
 }
