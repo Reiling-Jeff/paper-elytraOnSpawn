e6029260d59554cada57b4eef864a4d31c252cf6
Yuuto <Jasonehrlich72@gmail.com>
2024-12-21 23:30:42 +0100

    refactor: use plugins logger instead of System.out.print

--- src/main/java/de/questcraft/plugins/SoundMapper.java
+++ src/main/java/de/questcraft/plugins/SoundMapper.java
@@ -1,20 +1,22 @@
 package de.questcraft.plugins;
 
 import org.bukkit.Sound;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class SoundMapper {
     private static final Map<String, Sound> vanillaToBukkitMap = new HashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(SoundMapper.class);
 
     public static void initialize() {
         for (Sound bukkitSound : Sound.values()) {
             String vanillaSoundKey = bukkitSound.getKey().getKey();
             vanillaToBukkitMap.put(vanillaSoundKey, bukkitSound);
         }
-        // TODO Use plugins logger
-        System.out.println("SoundMapper initialized with " + vanillaToBukkitMap.size() + " sounds.");
+        SoundMapper.log.info("SoundMapper initialized with {} sounds.", vanillaToBukkitMap.size());
     }
 
     public static Sound getSound(String soundKey) {
